355a8030f135f5aecda92ccf1b5f661e020ebd43
matheusp1nhe1ro <matheus_pinheiro5@estudante.sesisenai.org.br>
2024-11-12 16:08:54 -0300

    Update JFgames.py

--- JFgames.py
+++ JFgames.py
@@ -19,7 +19,7 @@ def abrir_janela():
         'Xbox 360': 650,
         'Playstation 3': 520,
         'Nintendo Wii': 450,
-        'Nintendo WiiU': 234,
+        'Nintendo Wii U': 234,
         'Playstation 4 Slim + jogo': 1454,
         'Concord': 1000,
         'God of War Ragnarok': 250,
@@ -28,11 +28,11 @@ def abrir_janela():
         'Cabo USB 5.0': 80,
         'Guitarra para PS2': 560,
         "Fone Gamer": 740,
-        "Cartão Presente PSN R$ 30": 30,
-        "Cartão Presente PSN R$ 300": 300,
-        "Cartão Presente PSN R$ 350": 350,
-        "Cartão Presente PSN R$ 10": 10,
-        "Cartão Presente PSN R$ 110": 110,
+        "gift card PSN R$30": 30,
+        "gift card PSN R$300": 300,
+        "gift card PSN R$350": 350,
+        "gift card PSN R$10": 10,
+        "gift card PSN R$110": 110,
         "Super Nintendo ":450,
         "Nintendo 64":567,
         "DualSense":559.99,
@@ -46,7 +46,8 @@ def abrir_janela():
     moeda = ttk.Combobox(janela2, values=moedas)
     moeda.grid(row=2, column=0, sticky="w", padx=10)
 
-    # Função para mostrar o valor do produto
+    # Função mostrar o valor produto
+    mensagem_cotacao = tk.Label(janela2, text='')
     def mostrar_valor():
         produto_selecionado = moeda.get().strip()
         valor = produtos.get(produto_selecionado)
@@ -55,24 +56,26 @@ def abrir_janela():
         else:
             mensagem_cotacao["text"] = "Produto não encontrado"
         mensagem_cotacao.configure(bg='black', fg='white')
+        mensagem_cotacao.grid(row=5, column=0, sticky="w")
+      
 
     # Botão para mostrar o valor do produto
     botao_valor = tk.Button(janela2, text="Mostrar Valor", command=mostrar_valor)
-    botao_valor.grid(row=2, column=1, sticky="w", padx=10)
+    botao_valor.grid(row=3, column=0, sticky="w", padx=10)
     botao_valor.configure(fg='white', bg='black')
 
-    # Label para mensagens de cotação
-    mensagem_cotacao = tk.Label(janela2, text='')
-    mensagem_cotacao.grid(row=3, column=0, columnspan=2, sticky='w', padx=10)
 
+
+   
+    
     # Mensagem para reserva
-    mensagem3 = tk.Label(janela2, text="Caso você queira reservar um produto que não tenha em nossa loja, insira aqui:")
-    mensagem3.grid(row=4, column=0, sticky='w', padx=10, pady=(10, 0)) 
+    mensagem3 = tk.Label(janela2, text="insira aqui o produto para encomenda :")
+    mensagem3.grid(row=6, column=0, sticky='w', padx=10, pady=(10, 0)) 
     mensagem3.configure(fg='white', bg='black')
 
     # Caixa de texto para reserva
     caixa_texto = tk.Text(janela2, width=30, height=5)
-    caixa_texto.grid(row=5, column=0, columnspan=2, sticky="NSWE", padx=10)
+    caixa_texto.grid(row=8, column=0, columnspan=2, sticky="NSWE", padx=10)
 
     # Função para confirmar reserva
     def confirmar_reserva():
@@ -89,7 +92,7 @@ def abrir_janela():
 
     # Botão para confirmar a reserva
     botao_fechar = tk.Button(janela2, text="Confirmar reserva (TAXA DE R$15)", command=confirmar_reserva)
-    botao_fechar.grid(row=6, column=0, pady=10, sticky="w")
+    botao_fechar.grid(row=9, column=0, pady=10, sticky="w")
     botao_fechar.configure(fg='white', bg='black')
 
 # Janela principal
